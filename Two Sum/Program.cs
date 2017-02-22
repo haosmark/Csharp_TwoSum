9d6563f687a8c001242b3efddff793a960de349e
Denis St <haosmark@gmail.com>
2017-02-21 22:46:13 -0500

    Solution to two sum problem, with test cases

--- Two Sum/Program.cs
+++ Two Sum/Program.cs
@@ -1,15 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* 
+ * Test cases to the solution
+*/
+using System;
+using Xunit;
 
 namespace Two_Sum
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+        }
+
+        [Fact]
+        public void Test_3_2_4_TwoSum_6()
+        {
+            // Arrange
+            int target = 6;
+            int[] nums = new int[] { 3, 2, 4 };
+            int[] expected = new int[] { 1, 2 };
+
+            // Act
+            int[] actual = Solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_3_3_TwoSum_6()
+        {
+            // Arrange
+            int target = 6;
+            int[] nums = new int[] { 3, 3 };
+            int[] expected = new int[] { 0, 1 };
+
+            // Act
+            int[] actual = Solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_3_2_3TwoSum_6()
+        {
+            // Arrange
+            int target = 6;
+            int[] nums = new int[] { 3, 2, 3 };
+            int[] expected = new int[] { 0, 2 };
+
+            // Act
+            int[] actual = Solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_2_5_5_11_TwoSum_10()
+        {
+            // Arrange
+            int target = 10;
+            int[] nums = new int[] { 2, 5, 5, 11 };
+            int[] expected = new int[] { 1, 2 };
+
+            // Act
+            int[] actual = Solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_n3_4_3_90_TwoSum_0()
+        {
+            // Arrange
+            int target = 0;
+            int[] nums = new int[] { -3, 4, 3, 90 };
+            int[] expected = new int[] { 0, 2 };
+
+            // Act
+            int[] actual = Solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_5_75_25_TwoSum_100()
+        {
+            // Arrange
+            int target = 100;
+            int[] nums = new int[] { 5, 75, 25 };
+            int[] expected = new int[] { 1, 2 };
+
+            // Act
+            int[] actual = Solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
---
+++ Two Sum/Solution.cs
@@ -0,0 +1,58 @@
+﻿/* 
+ * Problem description: Given an array of integers, return indices of the two numbers such that 
+ * they add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+using System;
+
+namespace Two_Sum
+{
+    class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            // solve the problem by narrowing down the solution via indexes in a nested loop
+
+            // array must be sorted in order for this solution to work
+            // a copy of the original is needed to get the correct indexes back
+            int[] numsCopy = new int[nums.Length];
+            Array.Copy(nums, numsCopy, nums.Length);
+            Array.Sort(nums);
+
+            int j = nums.Length - 1;
+            // index from the left
+            for (int i = 0; i <= j; i++)
+            {
+                int sum = nums[i] + nums[j];
+
+                // keep decreasing the right index until you find a sum that's lower or equal
+                while (sum > target && j > i)
+                {
+                    j--;
+                    sum = nums[i] + nums[j];
+                }
+
+                // if the target is found, then get the original indexes and return
+                if (sum == target)
+                {
+                    int a = Array.IndexOf(numsCopy, nums[i]);
+                    int b = Array.LastIndexOf(numsCopy, nums[j]);
+
+                    // rearrange if needed
+                    if (a > b)
+                    {
+                        int c = a;
+                        a = b;
+                        b = c;
+                    }
+
+                    return new int[] { a, b };
+                }
+            }
+
+            return null;
+        }
+    }
+}
